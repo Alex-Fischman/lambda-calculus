7099cc3d43ef7128701e3eeb82643869b7df07b6
Alex <alex@fischman.org>
2019-12-02 19:59:07 -0800

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -2,18 +2,18 @@ import Lambda
 
 main :: IO ()
 main = do
-    print $ evaluate $ natural 1 -@- Main.not -@- bool True
+    print $ evaluate $ natural 4 -.- Main.not -.- bool True
 
 -- move these "library functions" somewhere else
 bool :: Bool -> Expr
-not :: Expr
 bool = Lam . Lam . Var . fromEnum
-not = Lam $ Var 0 -@- bool False -@- bool True
+not :: Expr
+not = Lam $ Var 0 -.- bool False -.- bool True
 
 natural :: Int -> Expr
 zero, incr :: Expr
 zero = Lam $ Lam $ Var 0
-incr = Lam $ Lam $ Lam $ Var 1 -$- Var 2 -@- Var 1 -@- Var 0
+incr = Lam $ Lam $ Lam $ Var 1 -$- Var 2 -.- Var 1 -.- Var 0
 natural n
     | n < 0  = zero
     | n == 0 = zero
